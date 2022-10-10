0d2f87208ddfdbfbf7c7b2a45fa9d06fd52a76c2
Amalpm7 <amal.p@nestgroup.net>
2022-10-10 12:52:39 +0530

    postap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PostappComponent } from './postapp/postapp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PostappComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/postapp/postapp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-postapp',
+  templateUrl: './postapp.component.html',
+  styleUrls: ['./postapp.component.css']
+})
+export class PostappComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
